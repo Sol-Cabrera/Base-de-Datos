88a53631b051b5829931d191b0b1bf6b2a418846
Sol-Cabrera <80895344+Sol-Cabrera@users.noreply.github.com>
2021-06-18 12:48:32 -0300

    Add files via upload

---
+++ Productos1.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE Producto;
+USE Producto;
+CREATE TABLE Productos1(IdProducto INT AUTO_INCREMENT PRIMARY KEY,
+nombre VARCHAR(50),
+precio DECIMAL(10.2),
+stock INT (11));
+SELECT* FROM Productos1;
